aba0051af5dc603ddbf068896dab357b58b3dc45
Joseph Blais <josephpierreblais@gmail.com>
2021-02-10 17:14:59 -0500

    completed function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,38 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'S', 'S', 'A', 'P', 'M', 'O', 'C'],
+    ], 'COMPASS')
+
+    assert.isTrue(result);
+  });
 });
+
+
+// loop through both arrays
+// if letter = first letter of word
+  // **check right diagonal
+  // while (array[x] < array.length && array[y] < array.height)
+    //  array[x]++
+    //array[x][y]++
+    // compare those to the equivalent letter in word
+
+    //[x]++
+    //[y]--
+
+    ///[x]--
+    //[y]++
+
+    //[x]--
+    //[y]--
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,65 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const newMatr = [];
+  for (let x = 0; x < matrix[0].length; x++) {
+    newMatr.push([]);
+    for (let y = 0; y < matrix.length; y++) {
+      newMatr[x].push(matrix[y][x]);
+      
     }
-}
-
-module.exports = wordSearch
+  }
+  return newMatr;
+};
+  
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    let reversedL = l.split("").reverse().join("");
+    if (l.includes(word) || reversedL.includes(word)) return true;
+  }
+  const verticalJoin = transpose(letters).map(lv => lv.join(''));
+  for (let v of verticalJoin) {
+    let reversedV = v.split("").reverse().join("");
+    if (v.includes(word)  || reversedV.includes(word)) return true;
+  }
+  
+  return false;
+};
+  
+  
+  
+const diagonals = (letters) => {
+  let length = letters.length;
+  let diagonalLines = (length + length) - 1;
+  let midPoint = (diagonalLines / 2) + 1;
+  let items = [];
+  let itemsInDiagonal = 0;
+  
+  for (let i = 1; i <= diagonalLines; i++) {
+    let rowIndex;
+    let columnIndex;
+    let itemSubarray = [];
+        
+  
+    if (i <= midPoint) {
+      itemsInDiagonal++;
+      for (let j = 0; j < itemsInDiagonal; j++) {
+        rowIndex = (i - j) - 1;
+        columnIndex = j;
+        itemSubarray.push(letters[rowIndex][columnIndex]);
+      }
+    } else {
+      itemsInDiagonal--;
+      for (let j = 0; j < itemsInDiagonal; j++) {
+        rowIndex = (length - 1) - j;
+        columnIndex = (i - length) + j;
+        itemSubarray.push(letters[rowIndex][columnIndex]);
+      }
+    }
+    items.push(itemSubarray);
+  }
+  console.log(items);
+};
+ 
+module.exports = wordSearch;
+  
+  
